f11ccf186b32b3fd6c7c8fe895e08c2827f4a7b6
Sriram Katta <sriram.katta@fau.de>
2023-12-06 17:44:39 +0100

    updated index to loc

--- mytensortest.cpp
+++ mytensortest.cpp
@@ -4,7 +4,15 @@
 int main()
 {
     Tensor<int> mat({2,2});
-    std::cout << mat << std::endl;
-    std::cout << "hello" << std::endl;
+    mat({0,0}) = 1;
+    mat({0,1}) = 2;
+    mat({1,0}) = 3;
+    mat({1,1}) = 4;
+
+    Tensor<int> mat3;
+
+    Tensor<int> mat2({3,3},1);
+
+    std::cout << (mat == mat2) << std::endl;
     return 0;
 }
